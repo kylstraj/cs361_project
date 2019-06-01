a3ea193454d59fd1728217612fc7d6cacfd77184
kylstraj <38022880+kylstraj@users.noreply.github.com>
2019-06-01 11:01:15 -0500

    Style modal (#5)
    * fix modal styling
    
    * format times

--- cs361/src/components/EventModal.js
+++ cs361/src/components/EventModal.js
@@ -1,26 +1,25 @@
 import React from 'react';
+import moment from 'moment';
+import { Modal } from 'react-bootstrap';
 
-const formatTime = ({ hour, minute }) => `${hour}:${minute}`;
+const formatTime = ({ hour, minute }) => moment(`${hour}${minute}`, 'HHmm').format('h:mm a');
 
 const EventModal = ({ close, remove, event: { title, date, location, startTime, endTime }}) => (
-  <div
-    style={{
-      position: 'absolute',
-      top: '50px',
-      left: '50px',
-      width: '80%',
-      border: 'solid 1px black',
-      zIndex: '1000',
-    }}
-  >
-    <h3>{title}</h3>
-    <div>{date.month} {date.day}, {date.year}</div>
-    <div>location: {location}</div>
-    <div>start: {formatTime(startTime)}</div>
-    <div>end: {formatTime(endTime)}</div>
-    <button onClick={close}>Close</button>
-    <button onClick={remove}>Remove</button>
-  </div>
+  <Modal show>
+    <Modal.Header>
+      <Modal.Title>{title}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <div>{date.month} {date.day}, {date.year}</div>
+      <div>location: {location}</div>
+      <div>start: {formatTime(startTime)}</div>
+      <div>end: {formatTime(endTime)}</div>
+    </Modal.Body>
+    <Modal.Footer>
+      <button onClick={close}>Close</button>
+      <button onClick={remove}>Remove</button>
+    </Modal.Footer>
+  </Modal>
 );
 
 export default EventModal;
