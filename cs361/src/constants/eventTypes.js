622a547bcd16fc730404b7fab0e9ab5d95997afa
James Kylstra <kylstraj@oregonstate.edu>
2019-06-03 22:24:42 -0500

    color events by type

--- cs361/src/constants/eventTypes.js
+++ cs361/src/constants/eventTypes.js
@@ -2,3 +2,10 @@ export const School = 'School';
 export const Extracurricular = 'Extracurricular';
 export const Finance = 'Finance';
 export const Work = 'Work';
+
+export const typeColors = {
+  [School]: 'blue',
+  [Extracurricular]: 'green',
+  [Finance]: 'black',
+  [Work]: 'purple',
+};
